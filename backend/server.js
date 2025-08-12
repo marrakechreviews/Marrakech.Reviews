3ae6b0e846d6c88ee126ab4175c03a1a2a4096cd
marrakechreviews <marrakechreviews@gmail.com>
2025-08-12 18:00:17 -0500

    fixes 1

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ app.use(helmet());
 
 // CORS configuration - More permissive for development
 app.use(cors({
-  origin: true,
+  origin: ["https://www.marrakech.reviews", "http://localhost:5000"],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   preflightContinue: false,
   optionsSuccessStatus: 200
 }));
