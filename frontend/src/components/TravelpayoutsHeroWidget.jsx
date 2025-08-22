d3f9e2420b65cfd62fde51db9595693aa5f4ac61
marrakechreviews <marrakechreviews@gmail.com>
2025-08-22 18:09:39 -0500

    colors flights

--- frontend/src/components/TravelpayoutsHeroWidget.jsx
+++ frontend/src/components/TravelpayoutsHeroWidget.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const TravelpayoutsHeroWidget = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "https://tpwdgt.com/content?currency=eur&trs=451574&shmarker=666986&combine_promos=101_7873&show_hotels=false&powered_by=true&locale=en&searchUrl=www.aviasales.com%2Fsearch&primary_override=%23f76500&color_button=%23f76500&color_icons=%23f76500&dark=%23000000ff&light=%23FFFFFF&secondary=%23FFFFFF00&special=%23000000ff&color_focused=%2332a8dd&border_radius=30&no_labels=true&plain=true&origin=BOD&destination=RAK&promo_id=7879&campaign_id=100";
+    script.src = "https://tpwdgt.com/content?currency=eur&trs=451574&shmarker=666986&combine_promos=101_7873&show_hotels=false&powered_by=true&locale=en&searchUrl=www.aviasales.com%2Fsearch&primary_override=%23F0EEECff&color_button=%23f76500&color_icons=%23F4F4F3ff&dark=%23000000ff&light=%23FFFFFF&secondary=%23FFFFFF00&special=%23FFFFFFff&color_focused=%23F76500&border_radius=30&no_labels=true&plain=true&origin=BOD&destination=RAK&promo_id=7879&campaign_id=100";
     // script.async = true;
     script.charset = "utf-8";
     
