e82dbefbbda1b2185dbf8333469c9e59ea0e2551
marrakechreviews <marrakechreviews@gmail.com>
2025-09-02 10:55:32 -0500

    side bare

--- admin/src/components/Layout/Sidebar.jsx
+++ admin/src/components/Layout/Sidebar.jsx
@@ -124,7 +124,7 @@ export default function Sidebar() {
       </div>
 
       {/* Navigation */}
-      <nav className="flex-1 p-4">
+      <nav className="flex-1 p-4 overflow-y-auto">
         <ul className="space-y-2">
           {navigation.map((item) => {
             const isActive = location.pathname === item.href;
